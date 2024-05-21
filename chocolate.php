aa30a6ccbf9120cc1af3a482c1fb8b001f69e82b
jopheth <jgcoralde.student@asiancollege.edu.ph>
2024-05-21 19:11:57 +0800

    chocolate.php

---
+++ chocolate.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHOCOLATE FACTORY</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar">
+    <ul>
+        <li><a href="chocolate">CHOCOLATE</a></li>
+        <li><a href="http://localhost/jopheth/adminpage.php">ADD NEW PRODUCT?....CLICK HERE</a></li>
+    </ul>
+</nav>
+<div class="container">
+    <div class="box">
+        <img src="/jopheth/img/chocolate.jpg" alt="KITKAT">
+        <h4>KITKAT</h4>
+        <h5>$50.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <img src="/jopheth/img/chocolate2.jpg" alt="Ghirardelli">
+        <h4>Ghirardelli</h4>
+        <h5>$200.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <img src="/jopheth/img/chocolate3.jpg" alt="Product">
+        <h4>Ferrero Rocher</h4>
+        <h5>$150.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+<div class="container">
+    <div class="box">
+        <img src="/jopheth/img/chocolate5.jpg" alt="SNIKERS">
+        <h4>SNIKERS</h4>
+        <h5>$50.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <img src="/jopheth/img/chocolate9.jpg" alt="Product">
+        <h4>Dairy milk</h4>
+        <h5>$100.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <img src="/jopheth/img/chocolate7.jpg" alt="Product">
+        <h4> Amul Chocolate</h4>
+        <h5>$120.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <img src="/jopheth/img/chocolate8.jpg" alt="Milka">
+        <h4>Milka</h4>
+        <h5>$90.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <img src="/jopheth/img/chocolate1.jpg" alt="HERSHEY'S">
+        <h4>HERSHEY'S</h4>
+        <h5>$250.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <img src="/jopheth/img/chocolate4.jpg" alt="TOBLERONE">
+        <h4>TOBLERONE</h4>
+        <h5>$60.00</h5>
+        <div class="cart">
+            <a href="#"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+        </div>
+    </div>
+</div>
+    <div class="adminbutton">
+        <a href="http://localhost/jopheth/adminpage.php">
+            <button>ADMIN PAGE</button>
+        </a>
+    </div>
+</div>
+</body>
+</html>
